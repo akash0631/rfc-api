845a60e9a071ccb71bec5a2f540956cb824d4e94
akash0631 <akash063@gmail.com>
2026-03-13 13:54:04 +0530

    Add ZADVANCE_PAYMENT_RFC controller [DEV] via RFC Portal

--- Controllers/Finance/ZADVANCE_PAYMENT_RFCController.cs
+++ Controllers/Finance/ZADVANCE_PAYMENT_RFCController.cs
@@ -17,91 +17,102 @@ namespace Vendor_SRM_Routing_Application.Controllers.PaperlessPicklist
     {
         [HttpPost]
         [Route("api/ZADVANCE_PAYMENT_RFC")]
-        public async Task<object> ZADVANCE_PAYMENT_RFC(ZADVANCE_PAYMENT_RFC_Request request)
+        public async Task<IHttpActionResult> GetAdvancePaymentDocuments(ZADVANCE_PAYMENT_RFCRequest request)
         {
             try
             {
-                RfcDestination rfcDestination = RfcDestinationManager.GetDestination(rfcConfigparameters);
-                RfcRepository rfcRepository = rfcDestination.Repository;
-                IRfcFunction rfcFunction = rfcRepository.CreateFunction("ZADVANCE_PAYMENT_RFC");
+                var destination = RfcDestinationManager.GetDestination(rfcConfigparameters("192.168.144.174", "210"));
+                var function = destination.Repository.CreateFunction("ZADVANCE_PAYMENT_RFC");
 
-                rfcFunction.SetValue("I_COMPANY_CODE", request.I_COMPANY_CODE);
-                rfcFunction.SetValue("I_POSTING_DATE_LOW", request.I_POSTING_DATE_LOW);
-                rfcFunction.SetValue("I_POSTING_DATE_HIGH", request.I_POSTING_DATE_HIGH);
+                // Set import parameters
+                function.SetValue("I_COMPANY_CODE", request.I_COMPANY_CODE);
+                function.SetValue("I_POSTING_DATE_LOW", request.I_POSTING_DATE_LOW);
+                function.SetValue("I_POSTING_DATE_HIGH", request.I_POSTING_DATE_HIGH);
 
-                rfcFunction.Invoke(rfcDestination);
+                // Invoke RFC
+                function.Invoke(destination);
 
-                IRfcStructure exReturn = rfcFunction.GetStructure("EX_RETURN");
-                if (exReturn != null && exReturn.GetValue("TYPE").ToString() == "E")
+                // Check return parameter
+                var exReturn = function.GetStructure("EX_RETURN");
+                if (exReturn != null && exReturn.GetString("TYPE") == "E")
                 {
-                    return new
+                    return Ok(new
                     {
                         Status = "E",
-                        Message = exReturn.GetValue("MESSAGE").ToString(),
-                        Data = new { ET_ADVANCE_PAYMENT = new object[0] }
-                    };
+                        Message = exReturn.GetString("MESSAGE"),
+                        Data = new { IT_FINAL = new List<object>() }
+                    });
                 }
 
-                IRfcTable etAdvancePaymentTable = rfcFunction.GetTable("ET_ADVANCE_PAYMENT");
-                List<Dictionary<string, object>> advancePaymentData = new List<Dictionary<string, object>>();
+                // Get IT_FINAL table
+                var itFinalTable = function.GetTable("IT_FINAL");
+                var resultList = new List<Dictionary<string, object>>();
 
-                for (int i = 0; i < etAdvancePaymentTable.RowCount; i++)
+                if (itFinalTable != null && itFinalTable.Count > 0)
                 {
-                    etAdvancePaymentTable.CurrentIndex = i;
-                    Dictionary<string, object> row = new Dictionary<string, object>();
-                    
-                    for (int j = 0; j < etAdvancePaymentTable.Metadata.FieldCount; j++)
+                    var metadata = itFinalTable.Metadata;
+                    var fieldNames = new List<string>();
+
+                    // Get field names, skip STRUCTURE/TABLE types
+                    for (int i = 0; i < metadata.FieldCount; i++)
                     {
-                        var field = etAdvancePaymentTable.Metadata[j];
+                        var field = metadata[i];
                         if (field.DataType != RfcDataType.STRUCTURE && field.DataType != RfcDataType.TABLE)
                         {
-                            row[field.Name] = etAdvancePaymentTable.GetValue(field.Name);
+                            fieldNames.Add(field.Name);
                         }
                     }
-                    advancePaymentData.Add(row);
+
+                    // Process each row
+                    foreach (IRfcStructure row in itFinalTable)
+                    {
+                        var rowData = new Dictionary<string, object>();
+                        foreach (var fieldName in fieldNames)
+                        {
+                            rowData[fieldName] = row.GetValue(fieldName)?.ToString() ?? "";
+                        }
+                        resultList.Add(rowData);
+                    }
                 }
 
-                return new
+                return Ok(new
                 {
                     Status = "S",
                     Message = "Success",
-                    Data = new
-                    {
-                        ET_ADVANCE_PAYMENT = advancePaymentData
-                    }
-                };
+                    Data = new { IT_FINAL = resultList }
+                });
             }
             catch (RfcAbapException ex)
             {
-                return new
+                return Ok(new
                 {
                     Status = "E",
                     Message = ex.Message,
-                    Data = new { ET_ADVANCE_PAYMENT = new object[0] }
-                };
+                    Data = new { IT_FINAL = new List<object>() }
+                });
             }
             catch (RfcCommunicationException ex)
             {
-                return new
+                return Ok(new
                 {
                     Status = "E",
                     Message = ex.Message,
-                    Data = new { ET_ADVANCE_PAYMENT = new object[0] }
-                };
+                    Data = new { IT_FINAL = new List<object>() }
+                });
             }
             catch (Exception ex)
             {
-                return new
+                return Ok(new
                 {
                     Status = "E",
                     Message = ex.Message,
-                    Data = new { ET_ADVANCE_PAYMENT = new object[0] }
-                };
+                    Data = new { IT_FINAL = new List<object>() }
+                });
             }
         }
     }
 
-    public class ZADVANCE_PAYMENT_RFC_Request
+    public class ZADVANCE_PAYMENT_RFCRequest
     {
         public string I_COMPANY_CODE { get; set; }
         public string I_POSTING_DATE_LOW { get; set; }
