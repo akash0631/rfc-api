fdbc3091b20df3080e809a1ff62790a69814f2e4
akash0631 <akash063@gmail.com>
2026-03-20 15:33:11 +0530

    Add ZPO_COST_UPD_RFC controller [DEV] via RFC Portal

--- Controllers/Finance/ZPO_COST_UPD_RFCController.cs
+++ Controllers/Finance/ZPO_COST_UPD_RFCController.cs
@@ -17,13 +17,13 @@ namespace Vendor_SRM_Routing_Application.Controllers.PaperlessPicklist
     {
         [HttpPost]
         [Route("api/ZPO_COST_UPD_RFC")]
-        public IHttpActionResult ZPO_COST_UPD_RFC([FromBody] ZPO_COST_UPD_RFCRequest request)
+        public async Task<IHttpActionResult> UpdatePurchaseOrderCost([FromBody] ZPO_COST_UPD_Request request)
         {
             try
             {
                 if (request == null || request.IM_DATA == null)
                 {
-                    return Ok(new { Status = "E", Message = "Request data cannot be null" });
+                    return Ok(new { Status = "E", Message = "Invalid request data" });
                 }
 
                 RfcConfigParameters rfcPar = BaseController.rfcConfigparameters();
@@ -31,38 +31,45 @@ namespace Vendor_SRM_Routing_Application.Controllers.PaperlessPicklist
                 RfcRepository rfcrep = dest.Repository;
                 IRfcFunction myfun = rfcrep.CreateFunction("ZPO_COST_UPD_RFC");
 
-                IRfcStructure imDataStructure = myfun.GetStructure("IM_DATA");
-                if (imDataStructure != null)
-                {
-                    foreach (var property in typeof(ZST_PO_IMP).GetProperties())
-                    {
-                        var value = property.GetValue(request.IM_DATA);
-                        if (value != null)
-                        {
-                            imDataStructure.SetValue(property.Name, value);
-                        }
-                    }
-                    myfun.SetValue("IM_DATA", imDataStructure);
-                }
+                IRfcStructure imData = myfun.GetStructure("IM_DATA");
+                if (!string.IsNullOrEmpty(request.IM_DATA.PO_NUMBER))
+                    imData.SetValue("PO_NUMBER", request.IM_DATA.PO_NUMBER);
+                if (!string.IsNullOrEmpty(request.IM_DATA.COMPANY_CODE))
+                    imData.SetValue("COMPANY_CODE", request.IM_DATA.COMPANY_CODE);
+                if (!string.IsNullOrEmpty(request.IM_DATA.PLANT))
+                    imData.SetValue("PLANT", request.IM_DATA.PLANT);
+                if (!string.IsNullOrEmpty(request.IM_DATA.VENDOR_CODE))
+                    imData.SetValue("VENDOR_CODE", request.IM_DATA.VENDOR_CODE);
+                if (!string.IsNullOrEmpty(request.IM_DATA.MATERIAL_CODE))
+                    imData.SetValue("MATERIAL_CODE", request.IM_DATA.MATERIAL_CODE);
+                if (request.IM_DATA.QUANTITY.HasValue)
+                    imData.SetValue("QUANTITY", request.IM_DATA.QUANTITY.Value);
+                if (request.IM_DATA.UNIT_PRICE.HasValue)
+                    imData.SetValue("UNIT_PRICE", request.IM_DATA.UNIT_PRICE.Value);
+                if (request.IM_DATA.TOTAL_COST.HasValue)
+                    imData.SetValue("TOTAL_COST", request.IM_DATA.TOTAL_COST.Value);
+                if (!string.IsNullOrEmpty(request.IM_DATA.CURRENCY))
+                    imData.SetValue("CURRENCY", request.IM_DATA.CURRENCY);
+                if (!string.IsNullOrEmpty(request.IM_DATA.UPDATED_BY))
+                    imData.SetValue("UPDATED_BY", request.IM_DATA.UPDATED_BY);
+                if (!string.IsNullOrEmpty(request.IM_DATA.UPDATE_DATE))
+                    imData.SetValue("UPDATE_DATE", request.IM_DATA.UPDATE_DATE);
+
+                myfun.SetValue("IM_DATA", imData);
 
                 myfun.Invoke(dest);
 
                 IRfcStructure EX_RETURN = myfun.GetStructure("EX_RETURN");
 
-                if (EX_RETURN != null)
+                string returnType = EX_RETURN.GetValue("TYPE")?.ToString() ?? "";
+                string returnMessage = EX_RETURN.GetValue("MESSAGE")?.ToString() ?? "";
+
+                if (returnType == "E")
                 {
-                    string returnType = EX_RETURN.GetString("TYPE");
-                    string returnMessage = EX_RETURN.GetString("MESSAGE");
-
-                    if (returnType == "E")
-                    {
-                        return Ok(new { Status = "E", Message = returnMessage });
-                    }
-
-                    return Ok(new { Status = "S", Message = returnMessage });
+                    return Ok(new { Status = "E", Message = returnMessage });
                 }
 
-                return Ok(new { Status = "S", Message = "Operation completed successfully" });
+                return Ok(new { Status = "S", Message = returnMessage });
             }
             catch (RfcAbapException ex)
             {
@@ -79,7 +86,7 @@ namespace Vendor_SRM_Routing_Application.Controllers.PaperlessPicklist
         }
     }
 
-    public class ZPO_COST_UPD_RFCRequest
+    public class ZPO_COST_UPD_Request
     {
         public ZST_PO_IMP IM_DATA { get; set; }
     }
@@ -87,14 +94,15 @@ namespace Vendor_SRM_Routing_Application.Controllers.PaperlessPicklist
     public class ZST_PO_IMP
     {
         public string PO_NUMBER { get; set; }
-        public string PO_ITEM { get; set; }
-        public decimal? COST_VALUE { get; set; }
-        public string CURRENCY { get; set; }
-        public string COST_TYPE { get; set; }
+        public string COMPANY_CODE { get; set; }
         public string PLANT { get; set; }
-        public string VENDOR { get; set; }
-        public string MATERIAL { get; set; }
-        public DateTime? EFFECTIVE_DATE { get; set; }
-        public string USER_ID { get; set; }
+        public string VENDOR_CODE { get; set; }
+        public string MATERIAL_CODE { get; set; }
+        public decimal? QUANTITY { get; set; }
+        public decimal? UNIT_PRICE { get; set; }
+        public decimal? TOTAL_COST { get; set; }
+        public string CURRENCY { get; set; }
+        public string UPDATED_BY { get; set; }
+        public string UPDATE_DATE { get; set; }
     }
 }
