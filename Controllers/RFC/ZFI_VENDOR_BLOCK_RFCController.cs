bda24119dd1a51d22aef846627456fe0f8cd0b80
akash0631 <akash063@gmail.com>
2026-03-25 15:14:52 +0530

    Add ZFI_VENDOR_BLOCK_RFC controller [DEV] via RFC Portal

---
+++ Controllers/RFC/ZFI_VENDOR_BLOCK_RFCController.cs
@@ -0,0 +1,68 @@
+using FMS_Fabric_Putway_Api.Models;
+using SAP.Middleware.Connector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Vendor_Application_MVC.Controllers;
+using Vendor_SRM_Routing_Application.Models.HU_Creation;
+using Vendor_SRM_Routing_Application.Models.PeperlessPicklist;
+
+namespace Vendor_SRM_Routing_Application.Controllers.PaperlessPicklist
+{
+    public class ZFI_VENDOR_BLOCK_RFCController : BaseController
+    {
+        [HttpPost]
+        [Route("api/ZFI_VENDOR_BLOCK_RFC")]
+        public IHttpActionResult BlockUnblockVendor(ZFI_VENDOR_BLOCK_RFCRequest request)
+        {
+            try
+            {
+                RfcConfigParameters rfcPar = BaseController.rfcConfigparameters();
+                RfcDestination dest = RfcDestinationManager.GetDestination(rfcPar);
+                RfcRepository rfcrep = dest.Repository;
+                IRfcFunction myfun = rfcrep.CreateFunction("ZFI_VENDOR_BLOCK_RFC");
+
+                myfun.SetValue("IV_LIFNR", request.IV_LIFNR);
+                myfun.SetValue("IV_BLOCK", request.IV_BLOCK);
+                myfun.SetValue("IV_BUKRS", request.IV_BUKRS);
+
+                myfun.Invoke(dest);
+
+                IRfcStructure EX_RETURN = myfun.GetStructure("EX_RETURN");
+
+                string returnType = EX_RETURN.GetString("TYPE");
+                string returnMessage = EX_RETURN.GetString("MESSAGE");
+
+                if (returnType == "E")
+                {
+                    return Ok(new { Status = "E", Message = returnMessage });
+                }
+
+                return Ok(new { Status = "S", Message = returnMessage });
+            }
+            catch (RfcAbapException ex)
+            {
+                return Ok(new { Status = "E", Message = ex.Message });
+            }
+            catch (CommunicationException ex)
+            {
+                return Ok(new { Status = "E", Message = ex.Message });
+            }
+            catch (Exception ex)
+            {
+                return Ok(new { Status = "E", Message = ex.Message });
+            }
+        }
+    }
+
+    public class ZFI_VENDOR_BLOCK_RFCRequest
+    {
+        public string IV_LIFNR { get; set; }
+        public string IV_BLOCK { get; set; }
+        public string IV_BUKRS { get; set; }
+    }
+}
