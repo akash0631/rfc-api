b57bc5088b25b671b4b6187135ccbcdef299a335
akash0631 <akash063@gmail.com>
2026-03-27 00:38:36 +0530

    Add ZFI_PI_DATA_RFC_TEST controller [DEV] via RFC Portal

---
+++ Controllers/Finance/ZFI_PI_DATA_RFC_TESTController.cs
@@ -0,0 +1,89 @@
+using SAP.Middleware.Connector;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Vendor_Application_MVC.Controllers;
+
+namespace Vendor_SRM_Routing_Application.Controllers.Finance
+{
+    public class ZFI_PI_DATA_RFC_TESTController : BaseController
+    {
+        [HttpPost]
+        [Route("api/ZFI_PI_DATA_RFC_TEST")]
+        public async Task<HttpResponseMessage> ExecuteZFI_PI_DATA_RFC_TEST(ZFI_PI_DATA_RFC_TESTRequest request)
+        {
+            try
+            {
+                if (request == null)
+                {
+                    return Request.CreateResponse(HttpStatusCode.BadRequest, new
+                    {
+                        Status = "E",
+                        Message = "Request cannot be null"
+                    });
+                }
+
+                RfcConfigParameters rfcPar = BaseController.rfcConfigparameters();
+                RfcDestination dest = RfcDestinationManager.GetDestination(rfcPar);
+                RfcRepository rfcrep = dest.Repository;
+                IRfcFunction myfun = rfcrep.CreateFunction("ZFI_PI_DATA_RFC_TEST");
+
+                myfun.SetValue("IM_TEST", request.IM_TEST ?? string.Empty);
+
+                myfun.Invoke(dest);
+
+                IRfcStructure EX_RETURN = myfun.GetStructure("EX_RETURN");
+
+                string returnType = EX_RETURN.GetString("TYPE");
+                string returnMessage = EX_RETURN.GetString("MESSAGE");
+
+                if (returnType == "E")
+                {
+                    return Request.CreateResponse(HttpStatusCode.BadRequest, new
+                    {
+                        Status = "E",
+                        Message = returnMessage
+                    });
+                }
+
+                return Request.CreateResponse(HttpStatusCode.OK, new
+                {
+                    Status = returnType,
+                    Message = returnMessage
+                });
+            }
+            catch (RfcAbapException ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, new
+                {
+                    Status = "E",
+                    Message = ex.Message
+                });
+            }
+            catch (RfcCommunicationException ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, new
+                {
+                    Status = "E",
+                    Message = ex.Message
+                });
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, new
+                {
+                    Status = "E",
+                    Message = ex.Message
+                });
+            }
+        }
+    }
+
+    public class ZFI_PI_DATA_RFC_TESTRequest
+    {
+        public string IM_TEST { get; set; }
+    }
+}
