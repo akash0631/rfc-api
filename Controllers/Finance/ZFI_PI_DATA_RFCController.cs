88351286249e8d36233bb2e700858d9c3e5c2a64
akash0631 <akash063@gmail.com>
2026-03-26 22:21:52 +0530

    fix: replace ZFI_PI_DATA_RFCController with correct reference code

--- Controllers/Finance/ZFI_PI_DATA_RFCController.cs
+++ Controllers/Finance/ZFI_PI_DATA_RFCController.cs
@@ -1,106 +1,36 @@
-using FMS_Fabric_Putway_Api.Models;
 using SAP.Middleware.Connector;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web.Http;
 using Vendor_Application_MVC.Controllers;
-using Vendor_SRM_Routing_Application.Models.HU_Creation;
-using Vendor_SRM_Routing_Application.Models.PeperlessPicklist;
-
-namespace Vendor_SRM_Routing_Application.Controllers.PaperlessPicklist
+namespace Vendor_SRM_Routing_Application.Controllers.Finance
 {
     public class ZFI_PI_DATA_RFCController : BaseController
-    {
-        [HttpPost]
-        [Route("api/ZFI_PI_DATA_RFC")]
-        public IHttpActionResult GetFinancePIData(ZFI_PI_DATA_RFC_Request request)
         {
-            try
-            {
-                RfcConfigParameters rfcPar = BaseController.rfcConfigparameters();
-                RfcDestination dest = RfcDestinationManager.GetDestination(rfcPar);
-                RfcRepository rfcrep = dest.Repository;
-                IRfcFunction myfun = rfcrep.CreateFunction("ZFI_PI_DATA_RFC");
-
-                myfun.SetValue("IT_POSTING_LOW", request.IT_POSTING_LOW);
-                myfun.SetValue("IT_POSTING_HIGH", request.IT_POSTING_HIGH);
-
-                myfun.Invoke(dest);
-
-                IRfcStructure EX_RETURN = myfun.GetStructure("EX_RETURN");
-
-                if (EX_RETURN.GetString("TYPE") == "E")
-                {
-                    return Ok(new
-                    {
-                        Status = "E",
-                        Message = EX_RETURN.GetString("MESSAGE"),
-                        Data = new { IT_FINAL = new List<object>() }
-                    });
-                }
-
-                IRfcTable tbl = myfun.GetTable("IT_FINAL");
-                var itFinalData = tbl.AsEnumerable().Select(row =>
-                {
-                    var rowData = new Dictionary<string, object>();
-                    var metadata = row.GetMetadata();
-                    
-                    for (int i = 0; i < metadata.FieldCount; i++)
-                    {
-                        var fieldMetadata = metadata[i];
-                        if (fieldMetadata.DataType != RfcDataType.STRUCTURE && fieldMetadata.DataType != RfcDataType.TABLE)
-                        {
-                            rowData[fieldMetadata.Name] = row.GetValue(fieldMetadata.Name);
-                        }
-                    }
-                    
-                    return rowData;
-                }).ToList();
-
-                return Ok(new
-                {
-                    Status = "S",
-                    Message = "Success",
-                    Data = new { IT_FINAL = itFinalData }
-                });
-            }
-            catch (RfcAbapException ex)
-            {
-                return Ok(new
-                {
-                    Status = "E",
-                    Message = ex.Message,
-                    Data = new { IT_FINAL = new List<object>() }
-                });
-            }
-            catch (RfcCommunicationException ex)
-            {
-                return Ok(new
-                {
-                    Status = "E",
-                    Message = ex.Message,
-                    Data = new { IT_FINAL = new List<object>() }
-                });
-            }
-            catch (Exception ex)
-            {
-                return Ok(new
-                {
-                    Status = "E",
-                    Message = ex.Message,
-                    Data = new { IT_FINAL = new List<object>() }
-                });
-            }
-        }
-    }
-
-    public class ZFI_PI_DATA_RFC_Request
-    {
-        public string IT_POSTING_LOW { get; set; }
-        public string IT_POSTING_HIGH { get; set; }
-    }
+                [HttpPost]
+                        public async Task<HttpResponseMessage> Post([FromBody] ZFI_PI_DATA_RFCRequest request)
+                                {
+                                            return await Task.Run(() =>
+                                                        {
+                                                                        try {
+                                                                                            if (request==null||request.IM_DATA==null||request.IM_DATA.Count==0)
+                                                                                                                    return Request.CreateResponse(HttpStatusCode.BadRequest, new { Status=false, Message="IM_DATA required." });
+                                                                                                                                        RfcConfigParameters rfcPar = BaseController.rfcConfigparameters();
+                                                                                                                                                            RfcDestination dest = RfcDestinationManager.GetDestination(rfcPar);
+                                                                                                                                                                                IRfcFunction myfun = dest.Repository.CreateFunction("ZFI_PI_DATA_RFC");
+                                                                                                                                                                                                    IRfcTable imData = myfun.GetTable("IM_DATA");
+                                                                                                                                                                                                                        foreach (var row in request.IM_DATA) { imData.Append(); foreach (var kv in row) imData.SetValue(kv.Key, kv.Value??string.Empty); }
+                                                                                                                                                                                                                                            myfun.Invoke(dest);
+                                                                                                                                                                                                                                                                string msgType = myfun.GetValue("MSG_TYPE")?.ToString()??string.Empty;
+                                                                                                                                                                                                                                                                                    string message = myfun.GetValue("MESSAGE")?.ToString()??string.Empty;
+                                                                                                                                                                                                                                                                                                        if (msgType=="E") return Request.CreateResponse(HttpStatusCode.BadRequest, new { Status=false, MsgType=msgType, Message=message });
+                                                                                                                                                                                                                                                                                                                            return Request.CreateResponse(HttpStatusCode.OK, new { Status=true, MsgType=msgType, Message=message });
+                                                                                                                                                                                                                                                                                                                                            } catch (Exception ex) { return Request.CreateResponse(HttpStatusCode.InternalServerError, new { Status=false, Message=ex.Message }); }
+                                                                                                                                                                                                                                                                                                                                                        });
+                                                                                                                                                                                                                                                                                                                                                                }
+                                                                                                                                                                                                                                                                                                                                                                    }
+                                                                                                                                                                                                                                                                                                                                                                        public class ZFI_PI_DATA_RFCRequest { public List<Dictionary<string,string>> IM_DATA { get; set; } }
 }
