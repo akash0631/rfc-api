9198edf00be7349963fecaf64bd8a3a2f9ecb670
akash0631 <akash063@gmail.com>
2026-03-27 08:40:49 +0530

    Add ZMM_ART_MOD_PO_RFC controller [DEV] via RFC Portal

--- Controllers/Vendor_SRM_Routing/ZMM_ART_MOD_PO_RFCController.cs
+++ Controllers/Vendor_SRM_Routing/ZMM_ART_MOD_PO_RFCController.cs
@@ -6,15 +6,16 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web.Http;
 using Vendor_Application_MVC.Controllers;
+using System.Linq;
+using System.ComponentModel.DataAnnotations;
 
 namespace Vendor_SRM_Routing_Application.Controllers.Vendor
 {
-    [RoutePrefix("api")]
     public class ZMM_ART_MOD_PO_RFCController : BaseController
     {
         [HttpPost]
-        [Route("ZMM_ART_MOD_PO_RFC")]
-        public HttpResponseMessage ZMM_ART_MOD_PO_RFC([FromBody] ZMM_ART_MOD_PO_RFCRequest request)
+        [Route("api/ZMM_ART_MOD_PO_RFC")]
+        public HttpResponseMessage ModifyArticleInPurchaseOrder([FromBody] ZMM_ART_MOD_PO_RFCRequest request)
         {
             try
             {
@@ -23,7 +24,22 @@ namespace Vendor_SRM_Routing_Application.Controllers.Vendor
                     return Request.CreateResponse(HttpStatusCode.BadRequest, new
                     {
                         Status = "E",
-                        Message = "Request cannot be null"
+                        Message = "Request body is required",
+                        Data = new { }
+                    });
+                }
+
+                if (!ModelState.IsValid)
+                {
+                    var errors = string.Join("; ", ModelState.Values
+                        .SelectMany(v => v.Errors)
+                        .Select(e => e.ErrorMessage));
+                    
+                    return Request.CreateResponse(HttpStatusCode.BadRequest, new
+                    {
+                        Status = "E",
+                        Message = errors,
+                        Data = new { }
                     });
                 }
 
@@ -32,104 +48,83 @@ namespace Vendor_SRM_Routing_Application.Controllers.Vendor
                 RfcRepository rfcrep = dest.Repository;
                 IRfcFunction myfun = rfcrep.CreateFunction("ZMM_ART_MOD_PO_RFC");
 
-                // Set IM_INPUT structure
-                if (request.IM_INPUT != null)
-                {
-                    IRfcStructure inputStruct = myfun.GetStructure("IM_INPUT");
-                    if (!string.IsNullOrEmpty(request.IM_INPUT.EBELN))
-                        inputStruct.SetValue("EBELN", request.IM_INPUT.EBELN);
-                    if (!string.IsNullOrEmpty(request.IM_INPUT.EBELP))
-                        inputStruct.SetValue("EBELP", request.IM_INPUT.EBELP);
-                    if (!string.IsNullOrEmpty(request.IM_INPUT.MATNR))
-                        inputStruct.SetValue("MATNR", request.IM_INPUT.MATNR);
-                    if (!string.IsNullOrEmpty(request.IM_INPUT.WERKS))
-                        inputStruct.SetValue("WERKS", request.IM_INPUT.WERKS);
-                    if (request.IM_INPUT.MENGE.HasValue)
-                        inputStruct.SetValue("MENGE", request.IM_INPUT.MENGE.Value);
-                    if (!string.IsNullOrEmpty(request.IM_INPUT.MEINS))
-                        inputStruct.SetValue("MEINS", request.IM_INPUT.MEINS);
-                    if (request.IM_INPUT.NETPR.HasValue)
-                        inputStruct.SetValue("NETPR", request.IM_INPUT.NETPR.Value);
-                    if (!string.IsNullOrEmpty(request.IM_INPUT.WAERS))
-                        inputStruct.SetValue("WAERS", request.IM_INPUT.WAERS);
-                    if (!string.IsNullOrEmpty(request.IM_INPUT.EINDT))
-                        inputStruct.SetValue("EINDT", request.IM_INPUT.EINDT);
-                    if (!string.IsNullOrEmpty(request.IM_INPUT.LGORT))
-                        inputStruct.SetValue("LGORT", request.IM_INPUT.LGORT);
-                }
-
-                // Set IM_OUTPUT structure
-                if (request.IM_OUTPUT != null)
-                {
-                    IRfcStructure outputStruct = myfun.GetStructure("IM_OUTPUT");
-                    if (!string.IsNullOrEmpty(request.IM_OUTPUT.EBELN))
-                        outputStruct.SetValue("EBELN", request.IM_OUTPUT.EBELN);
-                    if (!string.IsNullOrEmpty(request.IM_OUTPUT.EBELP))
-                        outputStruct.SetValue("EBELP", request.IM_OUTPUT.EBELP);
-                    if (!string.IsNullOrEmpty(request.IM_OUTPUT.MATNR))
-                        outputStruct.SetValue("MATNR", request.IM_OUTPUT.MATNR);
-                    if (!string.IsNullOrEmpty(request.IM_OUTPUT.WERKS))
-                        outputStruct.SetValue("WERKS", request.IM_OUTPUT.WERKS);
-                    if (request.IM_OUTPUT.MENGE.HasValue)
-                        outputStruct.SetValue("MENGE", request.IM_OUTPUT.MENGE.Value);
-                    if (!string.IsNullOrEmpty(request.IM_OUTPUT.MEINS))
-                        outputStruct.SetValue("MEINS", request.IM_OUTPUT.MEINS);
-                    if (request.IM_OUTPUT.NETPR.HasValue)
-                        outputStruct.SetValue("NETPR", request.IM_OUTPUT.NETPR.Value);
-                    if (!string.IsNullOrEmpty(request.IM_OUTPUT.WAERS))
-                        outputStruct.SetValue("WAERS", request.IM_OUTPUT.WAERS);
-                    if (!string.IsNullOrEmpty(request.IM_OUTPUT.EINDT))
-                        outputStruct.SetValue("EINDT", request.IM_OUTPUT.EINDT);
-                    if (!string.IsNullOrEmpty(request.IM_OUTPUT.LGORT))
-                        outputStruct.SetValue("LGORT", request.IM_OUTPUT.LGORT);
-                    if (!string.IsNullOrEmpty(request.IM_OUTPUT.LOEKZ))
-                        outputStruct.SetValue("LOEKZ", request.IM_OUTPUT.LOEKZ);
-                }
+                myfun.SetValue("IM_INPUT", request.IM_INPUT);
+                myfun.SetValue("IM_OUTPUT", request.IM_OUTPUT);
 
                 myfun.Invoke(dest);
 
                 IRfcStructure EX_RETURN = myfun.GetStructure("EX_RETURN");
 
-                string returnType = EX_RETURN.GetValue("TYPE")?.ToString() ?? "";
-                string returnMessage = EX_RETURN.GetValue("MESSAGE")?.ToString() ?? "";
-
-                if (returnType == "E")
+                if (EX_RETURN.GetValue("TYPE").ToString() == "E")
                 {
-                    return Request.CreateResponse(HttpStatusCode.BadRequest, new
+                    return Request.CreateResponse(HttpStatusCode.OK, new
                     {
                         Status = "E",
-                        Message = returnMessage
+                        Message = EX_RETURN.GetValue("MESSAGE").ToString(),
+                        Data = new { }
                     });
                 }
 
-                return Request.CreateResponse(HttpStatusCode.OK, new
+                IRfcTable tbl = myfun.GetTable("ET_PO_ARTICLE_MOD");
+
+                var tableData = tbl.AsEnumerable().Select(row =>
                 {
-                    Status = returnType == "S" ? "S" : "I",
-                    Message = returnMessage
-                });
+                    var rowData = new Dictionary<string, object>();
+                    for (int i = 0; i < row.FieldCount; i++)
+                    {
+                        var field = row.GetFieldMetadata(i);
+                        var fieldName = field.Name;
+                        var fieldValue = row.GetValue(fieldName);
+                        
+                        if (fieldValue != null)
+                        {
+                            rowData[fieldName] = fieldValue.ToString();
+                        }
+                        else
+                        {
+                            rowData[fieldName] = "";
+                        }
+                    }
+                    return rowData;
+                }).ToList();
+
+                var response = new
+                {
+                    Status = "S",
+                    Message = "Article/Material in Purchase Order modified successfully",
+                    Data = new
+                    {
+                        ET_PO_ARTICLE_MOD = tableData
+                    }
+                };
+
+                return Request.CreateResponse(HttpStatusCode.OK, response);
             }
             catch (RfcAbapException ex)
             {
-                return Request.CreateResponse(HttpStatusCode.InternalServerError, new
+                return Request.CreateResponse(HttpStatusCode.OK, new
                 {
                     Status = "E",
-                    Message = ex.Message
+                    Message = ex.Message,
+                    Data = new { }
                 });
             }
             catch (RfcCommunicationException ex)
             {
-                return Request.CreateResponse(HttpStatusCode.InternalServerError, new
+                return Request.CreateResponse(HttpStatusCode.OK, new
                 {
                     Status = "E",
-                    Message = ex.Message
+                    Message = ex.Message,
+                    Data = new { }
                 });
             }
             catch (Exception ex)
             {
-                return Request.CreateResponse(HttpStatusCode.InternalServerError, new
+                return Request.CreateResponse(HttpStatusCode.OK, new
                 {
                     Status = "E",
-                    Message = ex.Message
+                    Message = ex.Message,
+                    Data = new { }
                 });
             }
         }
@@ -137,36 +132,10 @@ namespace Vendor_SRM_Routing_Application.Controllers.Vendor
 
     public class ZMM_ART_MOD_PO_RFCRequest
     {
-        public ZMM_PO_ART_ST IM_INPUT { get; set; }
-        public ZMM_PO_ART_OUT_ST IM_OUTPUT { get; set; }
-    }
+        [Required(ErrorMessage = "IM_INPUT is required")]
+        public string IM_INPUT { get; set; }
 
-    public class ZMM_PO_ART_ST
-    {
-        public string EBELN { get; set; }
-        public string EBELP { get; set; }
-        public string MATNR { get; set; }
-        public string WERKS { get; set; }
-        public decimal? MENGE { get; set; }
-        public string MEINS { get; set; }
-        public decimal? NETPR { get; set; }
-        public string WAERS { get; set; }
-        public string EINDT { get; set; }
-        public string LGORT { get; set; }
-    }
-
-    public class ZMM_PO_ART_OUT_ST
-    {
-        public string EBELN { get; set; }
-        public string EBELP { get; set; }
-        public string MATNR { get; set; }
-        public string WERKS { get; set; }
-        public decimal? MENGE { get; set; }
-        public string MEINS { get; set; }
-        public decimal? NETPR { get; set; }
-        public string WAERS { get; set; }
-        public string EINDT { get; set; }
-        public string LGORT { get; set; }
-        public string LOEKZ { get; set; }
+        [Required(ErrorMessage = "IM_OUTPUT is required")]
+        public string IM_OUTPUT { get; set; }
     }
 }
