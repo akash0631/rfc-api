dec9919582ec0a9ae0e4c237de08340318c6e6f4
akash0631 <akash063@gmail.com>
2026-03-27 08:32:32 +0530

    Add ZFI_PI_DATA_RFC controller [DEV] via RFC Portal

--- Controllers/Finance/ZFI_PI_DATA_RFCController.cs
+++ Controllers/Finance/ZFI_PI_DATA_RFCController.cs
@@ -13,7 +13,7 @@ namespace Vendor_SRM_Routing_Application.Controllers.Finance
     {
         [HttpPost]
         [Route("api/ZFI_PI_DATA_RFC")]
-        public IHttpActionResult GetFinancialPostingData([FromBody] ZFI_PI_DATA_RFC_Request request)
+        public async Task<HttpResponseMessage> ProcessFinancePostingData(ZFI_PI_DATA_RFCRequest request)
         {
             try
             {
@@ -22,126 +22,130 @@ namespace Vendor_SRM_Routing_Application.Controllers.Finance
                 RfcRepository rfcrep = dest.Repository;
                 IRfcFunction myfun = rfcrep.CreateFunction("ZFI_PI_DATA_RFC");
 
+                IRfcTable itPostingLowTable = myfun.GetTable("IT_POSTING_LOW");
+                itPostingLowTable.Clear();
+
                 if (request.IT_POSTING_LOW != null)
                 {
-                    IRfcTable postingLowTable = myfun.GetTable("IT_POSTING_LOW");
-                    foreach (var item in request.IT_POSTING_LOW)
+                    foreach (var postingItem in request.IT_POSTING_LOW)
                     {
-                        postingLowTable.Append();
-                        var itemType = item.GetType();
-                        var properties = itemType.GetProperties();
-                        foreach (var prop in properties)
+                        itPostingLowTable.Append();
+                        IRfcStructure row = itPostingLowTable.CurrentRow;
+                        
+                        foreach (var property in typeof(PostingLowItem).GetProperties())
                         {
-                            var value = prop.GetValue(item);
+                            var value = property.GetValue(postingItem);
                             if (value != null)
                             {
-                                postingLowTable.SetValue(prop.Name.ToUpper(), value.ToString());
-                            }
-                        }
-                    }
-                }
-
-                if (request.IT_POSTING_HIGH != null)
-                {
-                    IRfcTable postingHighTable = myfun.GetTable("IT_POSTING_HIGH");
-                    foreach (var item in request.IT_POSTING_HIGH)
-                    {
-                        postingHighTable.Append();
-                        var itemType = item.GetType();
-                        var properties = itemType.GetProperties();
-                        foreach (var prop in properties)
-                        {
-                            var value = prop.GetValue(item);
-                            if (value != null)
-                            {
-                                postingHighTable.SetValue(prop.Name.ToUpper(), value.ToString());
+                                row.SetValue(property.Name.ToUpper(), value.ToString());
                             }
                         }
                     }
                 }
 
                 myfun.Invoke(dest);
-                
+
                 IRfcStructure EX_RETURN = myfun.GetStructure("EX_RETURN");
-                
-                if (EX_RETURN.GetString("TYPE") == "E")
+
+                string returnType = EX_RETURN.GetString("TYPE");
+                string returnMessage = EX_RETURN.GetString("MESSAGE");
+
+                if (returnType == "E")
                 {
-                    return Ok(new
+                    var errorResponse = new ZFI_PI_DATA_RFCResponse
                     {
                         Status = "E",
-                        Message = EX_RETURN.GetString("MESSAGE"),
-                        Data = new { IT_FINAL = new List<object>() }
-                    });
+                        Message = returnMessage
+                    };
+                    return Request.CreateResponse(HttpStatusCode.BadRequest, errorResponse);
                 }
 
-                IRfcTable finalTable = myfun.GetTable("IT_FINAL");
-                var finalData = new List<Dictionary<string, object>>();
-
-                if (finalTable.Count > 0)
+                var successResponse = new ZFI_PI_DATA_RFCResponse
                 {
-                    var metadata = finalTable.GetElementMetadata();
-                    
-                    foreach (IRfcStructure row in finalTable)
-                    {
-                        var rowData = new Dictionary<string, object>();
-                        
-                        for (int i = 0; i < metadata.FieldCount; i++)
-                        {
-                            var fieldMetadata = metadata[i];
-                            var fieldName = fieldMetadata.Name;
-                            var fieldType = fieldMetadata.DataType;
-                            
-                            if (fieldType != RfcDataType.STRUCTURE && fieldType != RfcDataType.TABLE)
-                            {
-                                var fieldValue = row.GetString(fieldName);
-                                rowData[fieldName] = fieldValue;
-                            }
-                        }
-                        
-                        finalData.Add(rowData);
-                    }
-                }
+                    Status = returnType,
+                    Message = returnMessage
+                };
 
-                return Ok(new
-                {
-                    Status = "S",
-                    Message = "Success",
-                    Data = new { IT_FINAL = finalData }
-                });
+                return Request.CreateResponse(HttpStatusCode.OK, successResponse);
             }
             catch (RfcAbapException ex)
             {
-                return Ok(new
+                var errorResponse = new ZFI_PI_DATA_RFCResponse
                 {
                     Status = "E",
-                    Message = ex.Message,
-                    Data = new { IT_FINAL = new List<object>() }
-                });
+                    Message = ex.Message
+                };
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, errorResponse);
             }
             catch (RfcCommunicationException ex)
             {
-                return Ok(new
+                var errorResponse = new ZFI_PI_DATA_RFCResponse
                 {
                     Status = "E",
-                    Message = ex.Message,
-                    Data = new { IT_FINAL = new List<object>() }
-                });
+                    Message = ex.Message
+                };
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, errorResponse);
             }
             catch (Exception ex)
             {
-                return Ok(new
+                var errorResponse = new ZFI_PI_DATA_RFCResponse
                 {
                     Status = "E",
-                    Message = ex.Message,
-                    Data = new { IT_FINAL = new List<object>() }
-                });
+                    Message = ex.Message
+                };
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, errorResponse);
             }
         }
     }
 
-    public class ZFI_PI_DATA_RFC_Request
+    public class ZFI_PI_DATA_RFCRequest
     {
-        public List<dynamic> IT_POSTING_LOW { get; set; }
-        public List<dynamic> IT_POSTING_HIGH { get; set; }
+        public List<PostingLowItem> IT_POSTING_LOW { get; set; }
+    }
+
+    public class ZFI_PI_DATA_RFCResponse
+    {
+        public string Status { get; set; }
+        public string Message { get; set; }
+    }
+
+    public class PostingLowItem
+    {
+        public string BUKRS { get; set; }
+        public string BLART { get; set; }
+        public string BLDAT { get; set; }
+        public string BUDAT { get; set; }
+        public string XBLNR { get; set; }
+        public string BKTXT { get; set; }
+        public string WAERS { get; set; }
+        public string KURSF { get; set; }
+        public string HKONT { get; set; }
+        public string KOSTL { get; set; }
+        public string AUFNR { get; set; }
+        public string WRBTR { get; set; }
+        public string DMBTR { get; set; }
+        public string MWSKZ { get; set; }
+        public string SGTXT { get; set; }
+        public string LIFNR { get; set; }
+        public string KUNNR { get; set; }
+        public string MATNR { get; set; }
+        public string WERKS { get; set; }
+        public string LGORT { get; set; }
+        public string MENGE { get; set; }
+        public string MEINS { get; set; }
+        public string EBELN { get; set; }
+        public string EBELP { get; set; }
+        public string VBELN { get; set; }
+        public string POSNR { get; set; }
+        public string PRCTR { get; set; }
+        public string SEGMENT { get; set; }
+        public string PSEGMENT { get; set; }
+        public string ZUONR { get; set; }
+        public string FKBER { get; set; }
+        public string XREF1 { get; set; }
+        public string XREF2 { get; set; }
+        public string XREF3 { get; set; }
+        public string BSCHL { get; set; }
+        public string SHKZG { get; set; }
     }
 }
