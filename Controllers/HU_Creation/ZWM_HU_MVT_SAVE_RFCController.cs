f90d760c6f31fc17d15806eeff54e19540a6bec3
akash0631 <akash063@gmail.com>
2026-03-27 14:30:13 +0530

    Add ZWM_HU_MVT_SAVE_RFC controller [DEV] via RFC Portal

---
+++ Controllers/HU_Creation/ZWM_HU_MVT_SAVE_RFCController.cs
@@ -0,0 +1,107 @@
+using SAP.Middleware.Connector;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Vendor_Application_MVC.Controllers;
+
+namespace Vendor_SRM_Routing_Application.Controllers.HUCreation
+{
+    public class ZWM_HU_MVT_SAVE_RFCController : BaseController
+    {
+        [HttpPost]
+        [Route("api/ZWM_HU_MVT_SAVE_RFC")]
+        public HttpResponseMessage ExecuteZWM_HU_MVT_SAVE_RFC(ZWM_HU_MVT_SAVE_RFCRequest request)
+        {
+            try
+            {
+                if (request == null)
+                {
+                    return Request.CreateResponse(HttpStatusCode.BadRequest, new
+                    {
+                        Status = "E",
+                        Message = "Request cannot be null"
+                    });
+                }
+
+                if (string.IsNullOrEmpty(request.IM_USER) || 
+                    string.IsNullOrEmpty(request.IM_PLANT) || 
+                    string.IsNullOrEmpty(request.IM_BIN) || 
+                    string.IsNullOrEmpty(request.IM_HU))
+                {
+                    return Request.CreateResponse(HttpStatusCode.BadRequest, new
+                    {
+                        Status = "E",
+                        Message = "All parameters (IM_USER, IM_PLANT, IM_BIN, IM_HU) are required"
+                    });
+                }
+
+                RfcConfigParameters rfcPar = BaseController.rfcConfigparameters();
+                RfcDestination dest = RfcDestinationManager.GetDestination(rfcPar);
+                RfcRepository rfcrep = dest.Repository;
+                IRfcFunction myfun = rfcrep.CreateFunction("ZWM_HU_MVT_SAVE_RFC");
+
+                myfun.SetValue("IM_USER", request.IM_USER);
+                myfun.SetValue("IM_PLANT", request.IM_PLANT);
+                myfun.SetValue("IM_BIN", request.IM_BIN);
+                myfun.SetValue("IM_HU", request.IM_HU);
+
+                myfun.Invoke(dest);
+
+                IRfcStructure EX_RETURN = myfun.GetStructure("EX_RETURN");
+
+                string returnType = EX_RETURN.GetString("TYPE");
+                string returnMessage = EX_RETURN.GetString("MESSAGE");
+
+                if (returnType == "E")
+                {
+                    return Request.CreateResponse(HttpStatusCode.OK, new
+                    {
+                        Status = "E",
+                        Message = returnMessage
+                    });
+                }
+
+                return Request.CreateResponse(HttpStatusCode.OK, new
+                {
+                    Status = "S",
+                    Message = !string.IsNullOrEmpty(returnMessage) ? returnMessage : "Operation completed successfully"
+                });
+            }
+            catch (RfcAbapException ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.OK, new
+                {
+                    Status = "E",
+                    Message = ex.Message
+                });
+            }
+            catch (RfcCommunicationException ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.OK, new
+                {
+                    Status = "E",
+                    Message = ex.Message
+                });
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.OK, new
+                {
+                    Status = "E",
+                    Message = ex.Message
+                });
+            }
+        }
+    }
+
+    public class ZWM_HU_MVT_SAVE_RFCRequest
+    {
+        public string IM_USER { get; set; }
+        public string IM_PLANT { get; set; }
+        public string IM_BIN { get; set; }
+        public string IM_HU { get; set; }
+    }
+}
