5515c7e46db782d9258b3a9891ae69aa51bcb5db
akash0631 <akash063@gmail.com>
2026-03-29 22:02:47 +0530

    Add ZMM_ART_MOD_PO_RFC controller [DEV] via RFC Portal

--- Controllers/Vendor_SRM_Routing/ZMM_ART_MOD_PO_RFCController.cs
+++ Controllers/Vendor_SRM_Routing/ZMM_ART_MOD_PO_RFCController.cs
@@ -9,18 +9,18 @@ using Vendor_Application_MVC.Controllers;
 
 namespace Vendor_SRM_Routing_Application.Controllers.Vendor
 {
-    [RoutePrefix("api")]
+    [Route("api/ZMM_ART_MOD_PO_RFC")]
     public class ZMM_ART_MOD_PO_RFCController : BaseController
     {
         [HttpPost]
-        [Route("ZMM_ART_MOD_PO_RFC")]
-        public async Task<HttpResponseMessage> ZMM_ART_MOD_PO_RFC([FromBody] ZMM_ART_MOD_PO_RFCRequest request)
+        [Route("api/ZMM_ART_MOD_PO_RFC")]
+        public HttpResponseMessage ZMM_ART_MOD_PO_RFC([FromBody] ZMM_ART_MOD_PO_RFCRequest request)
         {
             try
             {
                 if (request == null)
                 {
-                    return Request.CreateResponse(HttpStatusCode.BadRequest, new { Status = "E", Message = "Request body cannot be null" });
+                    return Request.CreateResponse(HttpStatusCode.BadRequest, new { Status = "E", Message = "Request body is null or invalid" });
                 }
 
                 RfcConfigParameters rfcPar = BaseController.rfcConfigparameters();
@@ -29,21 +29,35 @@ namespace Vendor_SRM_Routing_Application.Controllers.Vendor
                 IRfcFunction myfun = rfcrep.CreateFunction("ZMM_ART_MOD_PO_RFC");
 
                 // Set IM_INPUT parameter
-                if (request.IM_INPUT != null)
+                if (request.IM_INPUT != null && request.IM_INPUT.Count > 0)
                 {
-                    IRfcStructure inputStruct = myfun.GetStructure("IM_INPUT");
-                    if (!string.IsNullOrEmpty(request.IM_INPUT.EBELN))
-                        inputStruct.SetValue("EBELN", request.IM_INPUT.EBELN);
-                    if (!string.IsNullOrEmpty(request.IM_INPUT.EBELP))
-                        inputStruct.SetValue("EBELP", request.IM_INPUT.EBELP);
-                    if (!string.IsNullOrEmpty(request.IM_INPUT.MATNR))
-                        inputStruct.SetValue("MATNR", request.IM_INPUT.MATNR);
-                    if (!string.IsNullOrEmpty(request.IM_INPUT.COLOR))
-                        inputStruct.SetValue("COLOR", request.IM_INPUT.COLOR);
-                    if (request.IM_INPUT.MENGE.HasValue)
-                        inputStruct.SetValue("MENGE", request.IM_INPUT.MENGE.Value);
-                    if (request.IM_INPUT.NETPR.HasValue)
-                        inputStruct.SetValue("NETPR", request.IM_INPUT.NETPR.Value);
+                    IRfcTable inputTable = myfun.GetTable("IM_INPUT");
+                    foreach (var item in request.IM_INPUT)
+                    {
+                        inputTable.Append();
+                        if (!string.IsNullOrEmpty(item.EBELN))
+                            inputTable.SetValue("EBELN", item.EBELN);
+                        if (!string.IsNullOrEmpty(item.EBELP))
+                            inputTable.SetValue("EBELP", item.EBELP);
+                        if (!string.IsNullOrEmpty(item.MATNR))
+                            inputTable.SetValue("MATNR", item.MATNR);
+                        if (!string.IsNullOrEmpty(item.WERKS))
+                            inputTable.SetValue("WERKS", item.WERKS);
+                        if (!string.IsNullOrEmpty(item.LGORT))
+                            inputTable.SetValue("LGORT", item.LGORT);
+                        if (item.MENGE.HasValue)
+                            inputTable.SetValue("MENGE", item.MENGE.Value);
+                        if (item.NETPR.HasValue)
+                            inputTable.SetValue("NETPR", item.NETPR.Value);
+                        if (!string.IsNullOrEmpty(item.PEINH))
+                            inputTable.SetValue("PEINH", item.PEINH);
+                        if (!string.IsNullOrEmpty(item.WAERS))
+                            inputTable.SetValue("WAERS", item.WAERS);
+                        if (item.EINDT.HasValue)
+                            inputTable.SetValue("EINDT", item.EINDT.Value);
+                        if (!string.IsNullOrEmpty(item.COLOR))
+                            inputTable.SetValue("COLOR", item.COLOR);
+                    }
                 }
 
                 // Set IM_OUTPUT parameter
@@ -59,12 +73,22 @@ namespace Vendor_SRM_Routing_Application.Controllers.Vendor
                             outputTable.SetValue("EBELP", item.EBELP);
                         if (!string.IsNullOrEmpty(item.MATNR))
                             outputTable.SetValue("MATNR", item.MATNR);
-                        if (!string.IsNullOrEmpty(item.COLOR))
-                            outputTable.SetValue("COLOR", item.COLOR);
+                        if (!string.IsNullOrEmpty(item.WERKS))
+                            outputTable.SetValue("WERKS", item.WERKS);
+                        if (!string.IsNullOrEmpty(item.LGORT))
+                            outputTable.SetValue("LGORT", item.LGORT);
                         if (item.MENGE.HasValue)
                             outputTable.SetValue("MENGE", item.MENGE.Value);
                         if (item.NETPR.HasValue)
                             outputTable.SetValue("NETPR", item.NETPR.Value);
+                        if (!string.IsNullOrEmpty(item.PEINH))
+                            outputTable.SetValue("PEINH", item.PEINH);
+                        if (!string.IsNullOrEmpty(item.WAERS))
+                            outputTable.SetValue("WAERS", item.WAERS);
+                        if (item.EINDT.HasValue)
+                            outputTable.SetValue("EINDT", item.EINDT.Value);
+                        if (!string.IsNullOrEmpty(item.COLOR))
+                            outputTable.SetValue("COLOR", item.COLOR);
                         if (!string.IsNullOrEmpty(item.STATUS))
                             outputTable.SetValue("STATUS", item.STATUS);
                         if (!string.IsNullOrEmpty(item.MESSAGE))
@@ -102,28 +126,38 @@ namespace Vendor_SRM_Routing_Application.Controllers.Vendor
 
     public class ZMM_ART_MOD_PO_RFCRequest
     {
-        public ZMM_PO_ART_ST IM_INPUT { get; set; }
-        public List<ZMM_PO_ART_OUT_TT> IM_OUTPUT { get; set; }
+        public List<ZMM_PO_ART_Input> IM_INPUT { get; set; }
+        public List<ZMM_PO_ART_Output> IM_OUTPUT { get; set; }
     }
 
-    public class ZMM_PO_ART_ST
+    public class ZMM_PO_ART_Input
     {
         public string EBELN { get; set; }
         public string EBELP { get; set; }
         public string MATNR { get; set; }
-        public string COLOR { get; set; }
+        public string WERKS { get; set; }
+        public string LGORT { get; set; }
         public decimal? MENGE { get; set; }
         public decimal? NETPR { get; set; }
+        public string PEINH { get; set; }
+        public string WAERS { get; set; }
+        public DateTime? EINDT { get; set; }
+        public string COLOR { get; set; }
     }
 
-    public class ZMM_PO_ART_OUT_TT
+    public class ZMM_PO_ART_Output
     {
         public string EBELN { get; set; }
         public string EBELP { get; set; }
         public string MATNR { get; set; }
-        public string COLOR { get; set; }
+        public string WERKS { get; set; }
+        public string LGORT { get; set; }
         public decimal? MENGE { get; set; }
         public decimal? NETPR { get; set; }
+        public string PEINH { get; set; }
+        public string WAERS { get; set; }
+        public DateTime? EINDT { get; set; }
+        public string COLOR { get; set; }
         public string STATUS { get; set; }
         public string MESSAGE { get; set; }
     }
