bb12a664b680895a04860c6dc8f8d7b93ce4a23b
akash0631 <akash063@gmail.com>
2026-03-29 21:30:11 +0530

    Add ZMM_ART_MOD_PO_RFC controller [DEV] via RFC Portal

--- Controllers/Vendor_SRM_Routing/ZMM_ART_MOD_PO_RFCController.cs
+++ Controllers/Vendor_SRM_Routing/ZMM_ART_MOD_PO_RFCController.cs
@@ -13,13 +13,13 @@ namespace Vendor_SRM_Routing_Application.Controllers.Vendor
     {
         [HttpPost]
         [Route("api/ZMM_ART_MOD_PO_RFC")]
-        public HttpResponseMessage ZMM_ART_MOD_PO_RFC([FromBody] ZMM_ART_MOD_PO_Request request)
+        public async Task<HttpResponseMessage> ZMM_ART_MOD_PO_RFC([FromBody] ZMM_ART_MOD_PO_RFCRequest request)
         {
             try
             {
                 if (request == null)
                 {
-                    return Request.CreateResponse(HttpStatusCode.BadRequest, new { Status = "E", Message = "Request cannot be null" });
+                    return Request.CreateResponse(HttpStatusCode.BadRequest, new { Status = "E", Message = "Request body cannot be null" });
                 }
 
                 RfcConfigParameters rfcPar = BaseController.rfcConfigparameters();
@@ -34,15 +34,30 @@ namespace Vendor_SRM_Routing_Application.Controllers.Vendor
                     foreach (var item in request.IM_INPUT)
                     {
                         inputTable.Append();
-                        if (!string.IsNullOrEmpty(item.EBELN)) inputTable.SetValue("EBELN", item.EBELN);
-                        if (!string.IsNullOrEmpty(item.EBELP)) inputTable.SetValue("EBELP", item.EBELP);
-                        if (!string.IsNullOrEmpty(item.MATNR)) inputTable.SetValue("MATNR", item.MATNR);
-                        if (!string.IsNullOrEmpty(item.COLOR)) inputTable.SetValue("COLOR", item.COLOR);
-                        if (!string.IsNullOrEmpty(item.MENGE)) inputTable.SetValue("MENGE", item.MENGE);
-                        if (!string.IsNullOrEmpty(item.MEINS)) inputTable.SetValue("MEINS", item.MEINS);
-                        if (!string.IsNullOrEmpty(item.EEIND)) inputTable.SetValue("EEIND", item.EEIND);
-                        if (!string.IsNullOrEmpty(item.NETPR)) inputTable.SetValue("NETPR", item.NETPR);
-                        if (!string.IsNullOrEmpty(item.PEINH)) inputTable.SetValue("PEINH", item.PEINH);
+                        IRfcStructure inputRow = inputTable.CurrentRow;
+                        
+                        if (!string.IsNullOrEmpty(item.MANDT))
+                            inputRow.SetValue("MANDT", item.MANDT);
+                        if (!string.IsNullOrEmpty(item.EBELN))
+                            inputRow.SetValue("EBELN", item.EBELN);
+                        if (!string.IsNullOrEmpty(item.EBELP))
+                            inputRow.SetValue("EBELP", item.EBELP);
+                        if (!string.IsNullOrEmpty(item.MATNR))
+                            inputRow.SetValue("MATNR", item.MATNR);
+                        if (!string.IsNullOrEmpty(item.WERKS))
+                            inputRow.SetValue("WERKS", item.WERKS);
+                        if (!string.IsNullOrEmpty(item.LGORT))
+                            inputRow.SetValue("LGORT", item.LGORT);
+                        if (!string.IsNullOrEmpty(item.MENGE))
+                            inputRow.SetValue("MENGE", item.MENGE);
+                        if (!string.IsNullOrEmpty(item.MEINS))
+                            inputRow.SetValue("MEINS", item.MEINS);
+                        if (!string.IsNullOrEmpty(item.NETPR))
+                            inputRow.SetValue("NETPR", item.NETPR);
+                        if (!string.IsNullOrEmpty(item.PEINH))
+                            inputRow.SetValue("PEINH", item.PEINH);
+                        if (!string.IsNullOrEmpty(item.NETWR))
+                            inputRow.SetValue("NETWR", item.NETWR);
                     }
                 }
 
@@ -53,15 +68,34 @@ namespace Vendor_SRM_Routing_Application.Controllers.Vendor
                     foreach (var item in request.IM_OUTPUT)
                     {
                         outputTable.Append();
-                        if (!string.IsNullOrEmpty(item.EBELN)) outputTable.SetValue("EBELN", item.EBELN);
-                        if (!string.IsNullOrEmpty(item.EBELP)) outputTable.SetValue("EBELP", item.EBELP);
-                        if (!string.IsNullOrEmpty(item.MATNR)) outputTable.SetValue("MATNR", item.MATNR);
-                        if (!string.IsNullOrEmpty(item.COLOR)) outputTable.SetValue("COLOR", item.COLOR);
-                        if (!string.IsNullOrEmpty(item.MENGE)) outputTable.SetValue("MENGE", item.MENGE);
-                        if (!string.IsNullOrEmpty(item.MEINS)) outputTable.SetValue("MEINS", item.MEINS);
-                        if (!string.IsNullOrEmpty(item.EEIND)) outputTable.SetValue("EEIND", item.EEIND);
-                        if (!string.IsNullOrEmpty(item.NETPR)) outputTable.SetValue("NETPR", item.NETPR);
-                        if (!string.IsNullOrEmpty(item.PEINH)) outputTable.SetValue("PEINH", item.PEINH);
+                        IRfcStructure outputRow = outputTable.CurrentRow;
+                        
+                        if (!string.IsNullOrEmpty(item.MANDT))
+                            outputRow.SetValue("MANDT", item.MANDT);
+                        if (!string.IsNullOrEmpty(item.EBELN))
+                            outputRow.SetValue("EBELN", item.EBELN);
+                        if (!string.IsNullOrEmpty(item.EBELP))
+                            outputRow.SetValue("EBELP", item.EBELP);
+                        if (!string.IsNullOrEmpty(item.MATNR))
+                            outputRow.SetValue("MATNR", item.MATNR);
+                        if (!string.IsNullOrEmpty(item.WERKS))
+                            outputRow.SetValue("WERKS", item.WERKS);
+                        if (!string.IsNullOrEmpty(item.LGORT))
+                            outputRow.SetValue("LGORT", item.LGORT);
+                        if (!string.IsNullOrEmpty(item.MENGE))
+                            outputRow.SetValue("MENGE", item.MENGE);
+                        if (!string.IsNullOrEmpty(item.MEINS))
+                            outputRow.SetValue("MEINS", item.MEINS);
+                        if (!string.IsNullOrEmpty(item.NETPR))
+                            outputRow.SetValue("NETPR", item.NETPR);
+                        if (!string.IsNullOrEmpty(item.PEINH))
+                            outputRow.SetValue("PEINH", item.PEINH);
+                        if (!string.IsNullOrEmpty(item.NETWR))
+                            outputRow.SetValue("NETWR", item.NETWR);
+                        if (!string.IsNullOrEmpty(item.STATUS))
+                            outputRow.SetValue("STATUS", item.STATUS);
+                        if (!string.IsNullOrEmpty(item.MESSAGE))
+                            outputRow.SetValue("MESSAGE", item.MESSAGE);
                     }
                 }
 
@@ -69,60 +103,72 @@ namespace Vendor_SRM_Routing_Application.Controllers.Vendor
 
                 IRfcStructure EX_RETURN = myfun.GetStructure("EX_RETURN");
 
-                string returnType = EX_RETURN.GetValue("TYPE")?.ToString() ?? "";
-                string returnMessage = EX_RETURN.GetValue("MESSAGE")?.ToString() ?? "";
+                string returnType = EX_RETURN.GetValue("TYPE").ToString();
+                string returnMessage = EX_RETURN.GetValue("MESSAGE").ToString();
 
                 if (returnType == "E")
                 {
-                    return Request.CreateResponse(HttpStatusCode.OK, new { Status = "E", Message = returnMessage });
+                    return Request.CreateResponse(HttpStatusCode.BadRequest, new { Status = "E", Message = returnMessage });
                 }
 
-                return Request.CreateResponse(HttpStatusCode.OK, new { Status = "S", Message = returnMessage });
+                var response = new
+                {
+                    Status = returnType,
+                    Message = returnMessage
+                };
+
+                return Request.CreateResponse(HttpStatusCode.OK, response);
             }
             catch (RfcAbapException ex)
             {
-                return Request.CreateResponse(HttpStatusCode.OK, new { Status = "E", Message = ex.Message });
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, new { Status = "E", Message = ex.Message });
             }
             catch (RfcCommunicationException ex)
             {
-                return Request.CreateResponse(HttpStatusCode.OK, new { Status = "E", Message = ex.Message });
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, new { Status = "E", Message = ex.Message });
             }
             catch (Exception ex)
             {
-                return Request.CreateResponse(HttpStatusCode.OK, new { Status = "E", Message = ex.Message });
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, new { Status = "E", Message = ex.Message });
             }
         }
     }
 
-    public class ZMM_ART_MOD_PO_Request
+    public class ZMM_ART_MOD_PO_RFCRequest
     {
-        public List<ZMM_PO_ART_Item> IM_INPUT { get; set; }
-        public List<ZMM_PO_ART_OUT_Item> IM_OUTPUT { get; set; }
+        public List<ZMM_PO_ART_TT> IM_INPUT { get; set; }
+        public List<ZMM_PO_ART_OUT_TT> IM_OUTPUT { get; set; }
     }
 
-    public class ZMM_PO_ART_Item
+    public class ZMM_PO_ART_TT
     {
+        public string MANDT { get; set; }
         public string EBELN { get; set; }
         public string EBELP { get; set; }
         public string MATNR { get; set; }
-        public string COLOR { get; set; }
+        public string WERKS { get; set; }
+        public string LGORT { get; set; }
         public string MENGE { get; set; }
         public string MEINS { get; set; }
-        public string EEIND { get; set; }
         public string NETPR { get; set; }
         public string PEINH { get; set; }
+        public string NETWR { get; set; }
     }
 
-    public class ZMM_PO_ART_OUT_Item
+    public class ZMM_PO_ART_OUT_TT
     {
+        public string MANDT { get; set; }
         public string EBELN { get; set; }
         public string EBELP { get; set; }
         public string MATNR { get; set; }
-        public string COLOR { get; set; }
+        public string WERKS { get; set; }
+        public string LGORT { get; set; }
         public string MENGE { get; set; }
         public string MEINS { get; set; }
-        public string EEIND { get; set; }
         public string NETPR { get; set; }
         public string PEINH { get; set; }
+        public string NETWR { get; set; }
+        public string STATUS { get; set; }
+        public string MESSAGE { get; set; }
     }
 }
