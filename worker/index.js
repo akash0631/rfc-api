89a0997d0f4c70fb4a1f1cde6101fa346f525ab8
akash0631 <akash063@gmail.com>
2026-03-29 21:26:56 +0530

    fix: restore SAP_ENVS syntax broken by previous replace (stray h removed)

--- worker/index.js
+++ worker/index.js
@@ -7,7 +7,7 @@ const IIS_HOST         = 'https://sap-api.v2retail.net';
 const GH_WORKFLOW_ID   = '245504825';  // deploy-test-vm.yml
 const sleep = ms => new Promise(r => setTimeout(r, ms));
 const SAP_ENVS = {
-  dev:        { fn: 'rfcConfigparameters',           host: '192.168.144.174'h, client: '210' },
+  dev:        { fn: 'rfcConfigparameters',           host: '192.168.144.174', client: '210' },
   quality:    { fn: 'rfcConfigparametersquality',    host: '192.168.144.179', client: '600' },
   production: { fn: 'rfcConfigparametersproduction', host: '192.168.144.170', client: '600' },
 };
