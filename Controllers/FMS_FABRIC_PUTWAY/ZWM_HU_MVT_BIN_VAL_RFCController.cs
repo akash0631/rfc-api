d605d71e2c0d3f2fa82a6a003c9fa1b142492955
akash0631 <akash063@gmail.com>
2026-03-29 15:37:58 +0530

    Add ZWM_HU_MVT_BIN_VAL_RFC controller [DEV] via RFC Portal

---
+++ Controllers/FMS_FABRIC_PUTWAY/ZWM_HU_MVT_BIN_VAL_RFCController.cs
@@ -0,0 +1,120 @@
+using SAP.Middleware.Connector;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using Vendor_Application_MVC.Controllers;
+
+namespace Vendor_SRM_Routing_Application.Controllers.FabricPutway
+{
+    public class ZWM_HU_MVT_BIN_VAL_RFCController : BaseController
+    {
+        [HttpPost]
+        [Route("api/ZWM_HU_MVT_BIN_VAL_RFC")]
+        public HttpResponseMessage Execute(ZWM_HU_MVT_BIN_VAL_RFC_Request request)
+        {
+            try
+            {
+                if (request == null)
+                {
+                    return Request.CreateResponse(HttpStatusCode.BadRequest, new
+                    {
+                        Status = "E",
+                        Message = "Request cannot be null"
+                    });
+                }
+
+                if (string.IsNullOrEmpty(request.IM_USER))
+                {
+                    return Request.CreateResponse(HttpStatusCode.BadRequest, new
+                    {
+                        Status = "E",
+                        Message = "IM_USER is required"
+                    });
+                }
+
+                if (string.IsNullOrEmpty(request.IM_PLANT))
+                {
+                    return Request.CreateResponse(HttpStatusCode.BadRequest, new
+                    {
+                        Status = "E",
+                        Message = "IM_PLANT is required"
+                    });
+                }
+
+                if (string.IsNullOrEmpty(request.IM_BIN))
+                {
+                    return Request.CreateResponse(HttpStatusCode.BadRequest, new
+                    {
+                        Status = "E",
+                        Message = "IM_BIN is required"
+                    });
+                }
+
+                RfcConfigParameters rfcPar = BaseController.rfcConfigparameters();
+                RfcDestination dest = RfcDestinationManager.GetDestination(rfcPar);
+                RfcRepository rfcrep = dest.Repository;
+                IRfcFunction myfun = rfcrep.CreateFunction("ZWM_HU_MVT_BIN_VAL_RFC");
+
+                myfun.SetValue("IM_USER", request.IM_USER);
+                myfun.SetValue("IM_PLANT", request.IM_PLANT);
+                myfun.SetValue("IM_BIN", request.IM_BIN);
+
+                myfun.Invoke(dest);
+
+                IRfcStructure EX_RETURN = myfun.GetStructure("EX_RETURN");
+
+                string returnType = EX_RETURN.GetValue("TYPE").ToString();
+                string returnMessage = EX_RETURN.GetValue("MESSAGE").ToString();
+
+                if (returnType == "E")
+                {
+                    return Request.CreateResponse(HttpStatusCode.OK, new
+                    {
+                        Status = "E",
+                        Message = returnMessage
+                    });
+                }
+
+                return Request.CreateResponse(HttpStatusCode.OK, new
+                {
+                    Status = "S",
+                    Message = returnMessage
+                });
+            }
+            catch (RfcAbapException ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, new
+                {
+                    Status = "E",
+                    Message = ex.Message
+                });
+            }
+            catch (RfcCommunicationException ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, new
+                {
+                    Status = "E",
+                    Message = ex.Message
+                });
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, new
+                {
+                    Status = "E",
+                    Message = ex.Message
+                });
+            }
+        }
+    }
+
+    public class ZWM_HU_MVT_BIN_VAL_RFC_Request
+    {
+        public string IM_USER { get; set; }
+        public string IM_PLANT { get; set; }
+        public string IM_BIN { get; set; }
+    }
+}
