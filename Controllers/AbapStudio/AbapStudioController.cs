18fde3908f91cb4eeb8acb6e706155e6d65b2707
akash0631 <akash063@gmail.com>
2026-04-03 13:32:29 +0530

    fix: Add using Newtonsoft.Json.Linq for JObject in query-prod

--- Controllers/AbapStudio/AbapStudioController.cs
+++ Controllers/AbapStudio/AbapStudioController.cs
@@ -5,6 +5,7 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using Vendor_Application_MVC.Controllers;
 
 namespace Vendor_SRM_Routing_Application.Controllers.AbapStudio
