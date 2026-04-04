ebd42f2b1329dbc84fad833930ddad4ca91db6fa
akash0631 <akash063@gmail.com>
2026-04-04 13:04:48 +0530

    Add ZVEND_CODE_AUTH_RFC controller [DEV] via RFC Portal

--- Controllers/Vendor_SRM_Routing/ZVEND_CODE_AUTH_RFCController.cs
+++ Controllers/Vendor_SRM_Routing/ZVEND_CODE_AUTH_RFCController.cs
@@ -13,58 +13,91 @@ namespace Vendor_SRM_Routing_Application.Controllers.Vendor_SRM_Routing
     {
         [HttpPost]
         [Route("api/ZVEND_CODE_AUTH_RFC")]
-        public async Task<HttpResponseMessage> VendorCodeAuth([FromBody] VendorAuthRequest request)
+        public async Task<HttpResponseMessage> ZVEND_CODE_AUTH_RFC(ZVEND_CODE_AUTH_RFC_Request request)
         {
             return await Task.Run(() =>
             {
                 try
                 {
-                    if (request == null)
-                        return Request.CreateResponse(HttpStatusCode.OK, new { Status = "E", Message = "Request body cannot be null" });
-
-                    if (string.IsNullOrEmpty(request.IM_VEND_ID))
-                        return Request.CreateResponse(HttpStatusCode.OK, new { Status = "E", Message = "IM_VEND_ID is required" });
+                    if (string.IsNullOrEmpty(request.IM_USER_ID))
+                    {
+                        return Request.CreateResponse(HttpStatusCode.OK, new
+                        {
+                            Status = "E",
+                            Message = "IM_USER_ID is required"
+                        });
+                    }
 
                     if (string.IsNullOrEmpty(request.IM_PASSWORD))
-                        return Request.CreateResponse(HttpStatusCode.OK, new { Status = "E", Message = "IM_PASSWORD is required" });
+                    {
+                        return Request.CreateResponse(HttpStatusCode.OK, new
+                        {
+                            Status = "E",
+                            Message = "IM_PASSWORD is required"
+                        });
+                    }
 
                     RfcConfigParameters rfcPar = BaseController.rfcConfigparametersproduction();
                     RfcDestination dest = RfcDestinationManager.GetDestination(rfcPar);
                     RfcRepository rfcrep = dest.Repository;
                     IRfcFunction myfun = rfcrep.CreateFunction("ZVEND_CODE_AUTH_RFC");
 
-                    myfun.SetValue("IM_VEND_ID", request.IM_VEND_ID);
+                    myfun.SetValue("IM_USER_ID", request.IM_USER_ID);
                     myfun.SetValue("IM_PASSWORD", request.IM_PASSWORD);
+
                     myfun.Invoke(dest);
 
                     IRfcStructure EX_RETURN = myfun.GetStructure("EX_RETURN");
+                    
                     string returnType = EX_RETURN.GetString("TYPE");
                     string returnMessage = EX_RETURN.GetString("MESSAGE");
 
                     if (returnType == "E")
-                        return Request.CreateResponse(HttpStatusCode.OK, new { Status = "E", Message = returnMessage });
+                    {
+                        return Request.CreateResponse(HttpStatusCode.OK, new
+                        {
+                            Status = "E",
+                            Message = returnMessage
+                        });
+                    }
 
-                    return Request.CreateResponse(HttpStatusCode.OK, new { Status = "S", Message = returnMessage });
+                    return Request.CreateResponse(HttpStatusCode.OK, new
+                    {
+                        Status = "S",
+                        Message = returnMessage
+                    });
                 }
                 catch (RfcAbapException ex)
                 {
-                    return Request.CreateResponse(HttpStatusCode.OK, new { Status = "E", Message = ex.Message });
+                    return Request.CreateResponse(HttpStatusCode.OK, new
+                    {
+                        Status = "E",
+                        Message = ex.Message
+                    });
                 }
                 catch (RfcCommunicationException ex)
                 {
-                    return Request.CreateResponse(HttpStatusCode.OK, new { Status = "E", Message = ex.Message });
+                    return Request.CreateResponse(HttpStatusCode.OK, new
+                    {
+                        Status = "E",
+                        Message = ex.Message
+                    });
                 }
                 catch (Exception ex)
                 {
-                    return Request.CreateResponse(HttpStatusCode.OK, new { Status = "E", Message = ex.Message });
+                    return Request.CreateResponse(HttpStatusCode.OK, new
+                    {
+                        Status = "E",
+                        Message = ex.Message
+                    });
                 }
             });
         }
     }
 
-    public class VendorAuthRequest
+    public class ZVEND_CODE_AUTH_RFC_Request
     {
-        public string IM_VEND_ID { get; set; }
+        public string IM_USER_ID { get; set; }
         public string IM_PASSWORD { get; set; }
     }
 }
