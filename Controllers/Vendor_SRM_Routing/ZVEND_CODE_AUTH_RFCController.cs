e646f1c1869c722d2e0ca962a4e68efc2f03d252
V2-Application <147598886+V2-Application@users.noreply.github.com>
2026-04-04 12:51:30 +0530

    fix: rewrite ZVEND_CODE_AUTH_RFC with correct namespace, SAP RfcConfigParameters pattern and async Task return type

--- Controllers/Vendor_SRM_Routing/ZVEND_CODE_AUTH_RFCController.cs
+++ Controllers/Vendor_SRM_Routing/ZVEND_CODE_AUTH_RFCController.cs
@@ -7,74 +7,58 @@ using System.Threading.Tasks;
 using System.Web.Http;
 using Vendor_Application_MVC.Controllers;
 
-namespace VendorSrmRoutingApplication.Controllers
+namespace Vendor_SRM_Routing_Application.Controllers.Vendor_SRM_Routing
 {
     public class ZVEND_CODE_AUTH_RFCController : BaseController
     {
         [HttpPost]
         [Route("api/ZVEND_CODE_AUTH_RFC")]
-        public IHttpActionResult VendorCodeAuth([FromBody] VendorAuthRequest request)
+        public async Task<HttpResponseMessage> VendorCodeAuth([FromBody] VendorAuthRequest request)
         {
-            try
+            return await Task.Run(() =>
             {
-                if (request == null)
-                {
-                    return Ok(new { Status = "E", Message = "Request body cannot be null" });
-                }
-
-                if (string.IsNullOrEmpty(request.IM_VEND_ID))
-                {
-                    return Ok(new { Status = "E", Message = "IM_VEND_ID is required" });
-                }
-
-                if (string.IsNullOrEmpty(request.IM_PASSWORD))
-                {
-                    return Ok(new { Status = "E", Message = "IM_PASSWORD is required" });
-                }
-
-                RfcDestination connection = null;
                 try
                 {
-                    connection = SapRfcConnection.GetConnection("production");
-                    RfcRepository rfcrep = connection.Repository;
+                    if (request == null)
+                        return Request.CreateResponse(HttpStatusCode.OK, new { Status = "E", Message = "Request body cannot be null" });
+
+                    if (string.IsNullOrEmpty(request.IM_VEND_ID))
+                        return Request.CreateResponse(HttpStatusCode.OK, new { Status = "E", Message = "IM_VEND_ID is required" });
+
+                    if (string.IsNullOrEmpty(request.IM_PASSWORD))
+                        return Request.CreateResponse(HttpStatusCode.OK, new { Status = "E", Message = "IM_PASSWORD is required" });
+
+                    RfcConfigParameters rfcPar = BaseController.rfcConfigparametersproduction();
+                    RfcDestination dest = RfcDestinationManager.GetDestination(rfcPar);
+                    RfcRepository rfcrep = dest.Repository;
                     IRfcFunction myfun = rfcrep.CreateFunction("ZVEND_CODE_AUTH_RFC");
 
                     myfun.SetValue("IM_VEND_ID", request.IM_VEND_ID);
                     myfun.SetValue("IM_PASSWORD", request.IM_PASSWORD);
+                    myfun.Invoke(dest);
 
-                    myfun.Invoke(connection);
+                    IRfcStructure EX_RETURN = myfun.GetStructure("EX_RETURN");
+                    string returnType = EX_RETURN.GetString("TYPE");
+                    string returnMessage = EX_RETURN.GetString("MESSAGE");
 
-                IRfcStructure EX_RETURN = myfun.GetStructure("EX_RETURN");
+                    if (returnType == "E")
+                        return Request.CreateResponse(HttpStatusCode.OK, new { Status = "E", Message = returnMessage });
 
-                string returnType = EX_RETURN.GetString("TYPE");
-                string returnMessage = EX_RETURN.GetString("MESSAGE");
-
-                if (returnType == "E")
-                {
-                    return Ok(new { Status = "E", Message = returnMessage });
+                    return Request.CreateResponse(HttpStatusCode.OK, new { Status = "S", Message = returnMessage });
                 }
-
-                return Ok(new { Status = "S", Message = returnMessage });
-            }
-            catch (RfcAbapException ex)
-            {
-                return Ok(new { Status = "E", Message = ex.Message });
-            }
-            catch (RfcCommunicationException ex)
-            {
-                return Ok(new { Status = "E", Message = ex.Message });
-            }
-            catch (Exception ex)
-            {
-                return Ok(new { Status = "E", Message = ex.Message });
-            }
-            finally
-            {
-                if (connection != null)
+                catch (RfcAbapException ex)
                 {
-                    connection.Dispose();
+                    return Request.CreateResponse(HttpStatusCode.OK, new { Status = "E", Message = ex.Message });
                 }
-            }
+                catch (RfcCommunicationException ex)
+                {
+                    return Request.CreateResponse(HttpStatusCode.OK, new { Status = "E", Message = ex.Message });
+                }
+                catch (Exception ex)
+                {
+                    return Request.CreateResponse(HttpStatusCode.OK, new { Status = "E", Message = ex.Message });
+                }
+            });
         }
     }
 
