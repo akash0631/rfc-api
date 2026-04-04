520ee80ff296ebf433dcbd99020ebfbabeb352e7
akash0631 <akash063@gmail.com>
2026-04-04 11:41:00 +0530

    Add ZVEND_CODE_AUTH_RFC controller [DEV] via RFC Portal

--- Controllers/Vendor_SRM_Routing/ZVEND_CODE_AUTH_RFCController.cs
+++ Controllers/Vendor_SRM_Routing/ZVEND_CODE_AUTH_RFCController.cs
@@ -1,62 +1,86 @@
 using SAP.Middleware.Connector;
 using System;
+using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web.Http;
 using Vendor_Application_MVC.Controllers;
 
-namespace Vendor_SRM_Routing_Application.Controllers.Vendor_SRM_Routing
+namespace VendorSrmRoutingApplication.Controllers
 {
     public class ZVEND_CODE_AUTH_RFCController : BaseController
+    {
+        [HttpPost]
+        [Route("api/ZVEND_CODE_AUTH_RFC")]
+        public IHttpActionResult VendorCodeAuth([FromBody] VendorAuthRequest request)
         {
-                [HttpPost]
-                        [Route("api/ZVEND_CODE_AUTH_RFC")]
-                                public async Task<HttpResponseMessage> ZVEND_CODE_AUTH_RFC([FromBody] ZVEND_CODE_AUTH_RFCRequest request)
-                                        {
-                                                    return await Task.Run(() =>
-                                                                {
-                                                                                try
-                                                                                                {
-                                                                                                                    if (request == null)
-                                                                                                                                            return Request.CreateResponse(HttpStatusCode.OK, new { Status = "E", Message = "Request cannot be null" });
-                                                                                                                                            
-                                                                                                                                                                if (string.IsNullOrEmpty(request.IM_VEND_ID))
-                                                                                                                                                                                        return Request.CreateResponse(HttpStatusCode.OK, new { Status = "E", Message = "IM_VEND_ID is required" });
-                                                                                                                                                                                        
-                                                                                                                                                                                                            if (string.IsNullOrEmpty(request.IM_PASSWORD))
-                                                                                                                                                                                                                                    return Request.CreateResponse(HttpStatusCode.OK, new { Status = "E", Message = "IM_PASSWORD is required" });
-                                                                                                                                                                                                                                    
-                                                                                                                                                                                                                                                        RfcConfigParameters rfcPar = BaseController.rfcConfigparametersproduction();
-                                                                                                                                                                                                                                                                            RfcDestination dest = RfcDestinationManager.GetDestination(rfcPar);
-                                                                                                                                                                                                                                                                                                RfcRepository rfcrep = dest.Repository;
-                                                                                                                                                                                                                                                                                                                    IRfcFunction myfun = rfcrep.CreateFunction("ZVEND_CODE_AUTH_RFC");
-                                                                                                                                                                                                                                                                                                                    
-                                                                                                                                                                                                                                                                                                                                        myfun.SetValue("IM_VEND_ID", request.IM_VEND_ID);
-                                                                                                                                                                                                                                                                                                                                                            myfun.SetValue("IM_PASSWORD", request.IM_PASSWORD);
-                                                                                                                                                                                                                                                                                                                                                            
-                                                                                                                                                                                                                                                                                                                                                                                myfun.Invoke(dest);
-                                                                                                                                                                                                                                                                                                                                                                                
-                                                                                                                                                                                                                                                                                                                                                                                                    IRfcStructure EX_RETURN = myfun.GetStructure("EX_RETURN");
-                                                                                                                                                                                                                                                                                                                                                                                                                        string returnType = EX_RETURN.GetString("TYPE");
-                                                                                                                                                                                                                                                                                                                                                                                                                                            string returnMessage = EX_RETURN.GetString("MESSAGE");
-                                                                                                                                                                                                                                                                                                                                                                                                                                            
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                if (returnType == "E")
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        return Request.CreateResponse(HttpStatusCode.OK, new { Status = "E", Message = returnMessage });
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            return Request.CreateResponse(HttpStatusCode.OK, new { Status = "S", Message = returnMessage });
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            }
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            catch (Exception ex)
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            {
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                return Request.CreateResponse(HttpStatusCode.OK, new { Status = "E", Message = ex.Message });
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                }
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            });
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    }
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        }
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
-        public class ZVEND_CODE_AUTH_RFCRequest
-        {
-                    public string IM_VEND_ID { get; set; }
-                    public string IM_PASSWORD { get; set; }
+            try
+            {
+                if (request == null)
+                {
+                    return Ok(new { Status = "E", Message = "Request body cannot be null" });
+                }
+
+                if (string.IsNullOrEmpty(request.IM_VEND_ID))
+                {
+                    return Ok(new { Status = "E", Message = "IM_VEND_ID is required" });
+                }
+
+                if (string.IsNullOrEmpty(request.IM_PASSWORD))
+                {
+                    return Ok(new { Status = "E", Message = "IM_PASSWORD is required" });
+                }
+
+                RfcDestination connection = null;
+                try
+                {
+                    connection = SapRfcConnection.GetConnection("production");
+                    RfcRepository rfcrep = connection.Repository;
+                    IRfcFunction myfun = rfcrep.CreateFunction("ZVEND_CODE_AUTH_RFC");
+
+                    myfun.SetValue("IM_VEND_ID", request.IM_VEND_ID);
+                    myfun.SetValue("IM_PASSWORD", request.IM_PASSWORD);
+
+                    myfun.Invoke(connection);
+
+                IRfcStructure EX_RETURN = myfun.GetStructure("EX_RETURN");
+
+                string returnType = EX_RETURN.GetString("TYPE");
+                string returnMessage = EX_RETURN.GetString("MESSAGE");
+
+                if (returnType == "E")
+                {
+                    return Ok(new { Status = "E", Message = returnMessage });
+                }
+
+                return Ok(new { Status = "S", Message = returnMessage });
+            }
+            catch (RfcAbapException ex)
+            {
+                return Ok(new { Status = "E", Message = ex.Message });
+            }
+            catch (RfcCommunicationException ex)
+            {
+                return Ok(new { Status = "E", Message = ex.Message });
+            }
+            catch (Exception ex)
+            {
+                return Ok(new { Status = "E", Message = ex.Message });
+            }
+            finally
+            {
+                if (connection != null)
+                {
+                    connection.Dispose();
+                }
+            }
         }
+    }
+
+    public class VendorAuthRequest
+    {
+        public string IM_VEND_ID { get; set; }
+        public string IM_PASSWORD { get; set; }
+    }
 }
