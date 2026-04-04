a76633914e186c6c8c02a05eefb7576d18291b66
akash0631 <akash063@gmail.com>
2026-04-04 09:01:07 +0530

    Add ZVEND_CODE_AUTH_RFC controller [DEV] via RFC Portal

--- Controllers/Vendor_SRM_Routing/ZVEND_CODE_AUTH_RFCController.cs
+++ Controllers/Vendor_SRM_Routing/ZVEND_CODE_AUTH_RFCController.cs
@@ -5,68 +5,116 @@ using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web.Http;
-using Vendor_Application_MVC.Controllers;
+using VendorSrmRoutingApplication.Controllers;
 
-namespace Vendor_SRM_Routing_Application.Controllers.Vendor
+namespace VendorSrmRoutingApplication.Controllers
 {
     public class ZVEND_CODE_AUTH_RFCController : BaseController
     {
         [HttpPost]
         [Route("api/ZVEND_CODE_AUTH_RFC")]
-        public async Task<HttpResponseMessage> ZVEND_CODE_AUTH_RFC([FromBody] ZVEND_CODE_AUTH_RFCRequest request)
+        public HttpResponseMessage ZVEND_CODE_AUTH_RFC([FromBody] ZVEND_CODE_AUTH_RFCRequest request)
         {
+            IRfcConnection connection = null;
+            
             try
             {
                 if (request == null)
                 {
-                    return Request.CreateResponse(HttpStatusCode.BadRequest, new { Status = "E", Message = "Request cannot be null" });
+                    return Request.CreateResponse(HttpStatusCode.OK, new
+                    {
+                        Status = "E",
+                        Message = "Request cannot be null"
+                    });
                 }
 
-                if (string.IsNullOrEmpty(request.IM_ASSOC_ID) || string.IsNullOrEmpty(request.IM_PASSWORD))
+                if (string.IsNullOrEmpty(request.IM_VEND_ID))
                 {
-                    return Request.CreateResponse(HttpStatusCode.BadRequest, new { Status = "E", Message = "IM_ASSOC_ID and IM_PASSWORD are required" });
+                    return Request.CreateResponse(HttpStatusCode.OK, new
+                    {
+                        Status = "E",
+                        Message = "IM_VEND_ID is required"
+                    });
                 }
 
-                RfcConfigParameters rfcPar = BaseController.rfcConfigparameters();
-                RfcDestination dest = RfcDestinationManager.GetDestination(rfcPar);
-                RfcRepository rfcrep = dest.Repository;
-                IRfcFunction myfun = rfcrep.CreateFunction("ZVEND_CODE_AUTH_RFC");
+                if (string.IsNullOrEmpty(request.IM_PASSWORD))
+                {
+                    return Request.CreateResponse(HttpStatusCode.OK, new
+                    {
+                        Status = "E",
+                        Message = "IM_PASSWORD is required"
+                    });
+                }
 
-                myfun.SetValue("IM_ASSOC_ID", request.IM_ASSOC_ID);
+                var connection = SapRfcConnection.GetConnection("production");
+                IRfcFunction myfun = connection.CreateFunction("ZVEND_CODE_AUTH_RFC");
+
+                myfun.SetValue("IM_VEND_ID", request.IM_VEND_ID);
                 myfun.SetValue("IM_PASSWORD", request.IM_PASSWORD);
 
-                myfun.Invoke(dest);
+                myfun.Invoke(connection);
 
                 IRfcStructure EX_RETURN = myfun.GetStructure("EX_RETURN");
-
+                
                 string returnType = EX_RETURN.GetString("TYPE");
                 string returnMessage = EX_RETURN.GetString("MESSAGE");
 
                 if (returnType == "E")
                 {
-                    return Request.CreateResponse(HttpStatusCode.BadRequest, new { Status = "E", Message = returnMessage });
+                    return Request.CreateResponse(HttpStatusCode.OK, new
+                    {
+                        Status = "E",
+                        Message = returnMessage
+                    });
                 }
 
-                return Request.CreateResponse(HttpStatusCode.OK, new { Status = returnType, Message = returnMessage });
+                return Request.CreateResponse(HttpStatusCode.OK, new
+                {
+                    Status = "S",
+                    Message = returnMessage
+                });
             }
             catch (RfcAbapException ex)
             {
-                return Request.CreateResponse(HttpStatusCode.InternalServerError, new { Status = "E", Message = ex.Message });
+                return Request.CreateResponse(HttpStatusCode.OK, new
+                {
+                    Status = "E",
+                    Message = ex.Message
+                });
             }
             catch (RfcCommunicationException ex)
             {
-                return Request.CreateResponse(HttpStatusCode.InternalServerError, new { Status = "E", Message = ex.Message });
+                return Request.CreateResponse(HttpStatusCode.OK, new
+                {
+                    Status = "E",
+                    Message = ex.Message
+                });
             }
             catch (Exception ex)
             {
-                return Request.CreateResponse(HttpStatusCode.InternalServerError, new { Status = "E", Message = ex.Message });
+                return Request.CreateResponse(HttpStatusCode.OK, new
+                {
+                    Status = "E",
+                    Message = ex.Message
+                });
+            }
+            finally
+            {
+                if (connection != null)
+                {
+                    try
+                    {
+                        connection.Dispose();
+                    }
+                    catch { }
+                }
             }
         }
     }
 
     public class ZVEND_CODE_AUTH_RFCRequest
     {
-        public string IM_ASSOC_ID { get; set; }
+        public string IM_VEND_ID { get; set; }
         public string IM_PASSWORD { get; set; }
     }
 }
